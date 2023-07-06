19ae28503667c75595f267a347b0fbbd545279c4
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2023-07-06 20:39:32 +0700

    chore: change name to WAFO

--- pages/register.js
+++ pages/register.js
@@ -30,7 +30,7 @@ const Register = () => {
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
         >
-          YukJahit
+          WAFO
         </a>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
